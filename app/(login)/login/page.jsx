68273faf2600c264ce50a6e503caee2bf8ceadc7
Enayet <enayetflweb@gamil.com>
2024-01-29 18:42:09 +0600

    change redirection from login page

--- app/(login)/login/page.jsx
+++ app/(login)/login/page.jsx
@@ -1,17 +1,17 @@
 import LoginForm from '@/app/_components/LoginForm'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
-import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
+// import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+// import { getServerSession } from 'next-auth'
+// import { redirect } from 'next/navigation'
 import React from 'react'
 
 const Login = async() => {
-  const session = await getServerSession(authOptions)
+  // const session = await getServerSession(authOptions)
 
-  console.log(session)
+  // console.log(session)
   // console.log(session?.status)
-  if(session?.user){
-    redirect('/')
-  }
+  // if(session?.user){
+  //   redirect('/')
+  // }
 
   return (
     <div>
