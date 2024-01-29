d09d0afa62144fa3b34ee718a969edea888f8ab9
Enayet <enayetflweb@gamil.com>
2024-01-29 11:19:47 +0600

    middleware updated

--- middleware.js
+++ middleware.js
@@ -1,23 +1,23 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
 export {default} from "next-auth/middleware"
 
-export function middleware(request){
-  const path = request.nextUrl.pathname;
-  const isPublicPath = path === "/login" || path === "/register"
-  // console.log(path)
-  const token = request.cookies.get('__Secure-next-auth.session-token')?.value || ''
+// export function middleware(request){
+//   const path = request.nextUrl.pathname;
+//   const isPublicPath = path === "/login" || path === "/register"
+//   // console.log(path)
+//   const token = request.cookies.get('__Secure-next-auth.session-token')?.value || ''
 
-  __Secure-next-auth.session-token
+//   __Secure-next-auth.session-token
 
-  if(isPublicPath && token){
-    return NextResponse.redirect(new URL('/client', request.nextUrl))
-  }
-  if(!isPublicPath && !token){
-    return NextResponse.redirect(new URL('/login', request.nextUrl))
-  }
+//   if(isPublicPath && token){
+//     return NextResponse.redirect(new URL('/client', request.nextUrl))
+//   }
+//   if(!isPublicPath && !token){
+//     return NextResponse.redirect(new URL('/login', request.nextUrl))
+//   }
 
-  // console.log(token)
-}
+//   // console.log(token)
+// }
 
-export const config = {matcher: ["/client/:path*", "/login", "/register"]}
+export const config = {matcher: ["/client/:path*"]}
