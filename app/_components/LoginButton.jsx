cf0c5561cd56c9647145ec1b4f8f3db531acca65
Enayet <enayetflweb@gamil.com>
2024-01-29 11:15:37 +0600

    login route changed

--- app/_components/LoginButton.jsx
+++ app/_components/LoginButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const LoginButton = ({text}) => {
   return (
-    <Link href={'/login'}>
+    <Link href={'https://auth-testing-mu.vercel.app/login'}>
     <button className='px-4 py-2 bg-slate-500 text-white font-semibold text-lg rounded-lg shadow-xl'>{text}</button>
     </Link>
   )
