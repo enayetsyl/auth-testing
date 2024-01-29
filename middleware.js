718b836d0cdc420e0c7e0fa866b30a8bd6074d2d
Enayet <enayetflweb@gamil.com>
2024-01-29 11:03:49 +0600

    token rechange

--- middleware.js
+++ middleware.js
@@ -6,7 +6,9 @@ export function middleware(request){
   const path = request.nextUrl.pathname;
   const isPublicPath = path === "/login" || path === "/register"
   // console.log(path)
-  const token = request.cookies.get('_Secure-next-auth.session-token')?.value || ''
+  const token = request.cookies.get('__Secure-next-auth.session-token')?.value || ''
+
+  __Secure-next-auth.session-token
 
   if(isPublicPath && token){
     return NextResponse.redirect(new URL('/client', request.nextUrl))
